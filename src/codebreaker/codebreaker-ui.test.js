807f9b21d2550e9be74ead3380ebaa7a098e66b4
Israel Antezana Rojas <israelantezana@gmail.com>
2022-06-29 23:16:53 -0400

    Siempre gana

--- src/codebreaker/codebreaker-ui.test.js
+++ src/codebreaker/codebreaker-ui.test.js
@@ -6,4 +6,14 @@ describe("Codebreaker UI", () => {
     const h1 = document.querySelector("h1");
     expect(h1.innerHTML).toEqual("Bienvenido al codebreaker");
   });
+
+  it("adivino con codigo ganador", () => {
+    document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+    const numero = document.querySelector("#numero");
+    const form = document.querySelector("#adivinar-submit");
+    const resultado = document.querySelector("#resultado");
+    numero.value = "5";
+    form.click();
+    expect(resultado.innerHTML).toEqual("Ganaste!");
+  });
 });
