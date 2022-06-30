a6c41d4719324917861e9cd1d8b2e898214fa04b
Israel Antezana Rojas <israelantezana@gmail.com>
2022-06-29 23:19:08 -0400

    Refactoring tests

--- src/codebreaker/codebreaker-ui.test.js
+++ src/codebreaker/codebreaker-ui.test.js
@@ -1,14 +1,15 @@
 import fs from "fs";
 
 describe("Codebreaker UI", () => {
-  it("muestra un mensaje de bienvenida", () => {
+  beforeAll(() => {
     document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+  });
+  it("muestra un mensaje de bienvenida", () => {
     const h1 = document.querySelector("h1");
     expect(h1.innerHTML).toEqual("Bienvenido al codebreaker");
   });
 
   it("adivino con codigo ganador", () => {
-    document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
     const numero = document.querySelector("#numero");
     const form = document.querySelector("#adivinar-submit");
     const resultado = document.querySelector("#resultado");
